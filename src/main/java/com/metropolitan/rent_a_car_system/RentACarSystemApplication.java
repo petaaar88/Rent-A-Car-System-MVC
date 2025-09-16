ea40438c499c3c4c0d45a8db15954c71e289ec7a
petaaar88 <pe.bn80@gmail.com>
2025-09-16 17:48:01 +0200

    Initial commit

---
+++ src/main/java/com/metropolitan/rent_a_car_system/RentACarSystemApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.rent_a_car_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentACarSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/rent_a_car_system/RentACarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.rent_a_car_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
